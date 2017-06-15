9a8c6e2b8268c05bf0fba22dfbbbd48f5228c5fe
hao.liu <hao.liu@hpe.com>
2017-06-15 13:08:57 +0800

    receive message

---
+++ hello-rabbitmq/src/main/java/com/hank/rabbitmq/hello/Recv.java
@@ -0,0 +1,40 @@
+package com.hank.rabbitmq.hello;
+
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * Created by Hank on 6/15/2017.
+ */
+public class Recv {
+
+    private final static String QUEUE_NAME = "hello";
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        // create connection and channel
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        // declare queue and publish message
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+        // Consumer is a callback in the form of an object that will buffer the messages until we're ready to use them
+        Consumer consumer = new DefaultConsumer(channel) {
+            // the method to handle received messages
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body)
+                    throws IOException {
+                super.handleDelivery(consumerTag, envelope, properties, body);
+                String message = new String(body, "UTF-8");
+                System.out.println(" [x] Received '" + message + "'");
+            }
+        };
+
+        channel.basicConsume(QUEUE_NAME, true, consumer);
+    }
+}
