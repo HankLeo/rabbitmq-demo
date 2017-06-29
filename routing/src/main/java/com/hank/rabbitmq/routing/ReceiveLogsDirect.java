43c8043e568572e09d880d871516dcfe13992e5c
hao.liu <hao.liu@hpe.com>
2017-06-29 15:48:55 +0800

    routing

--- routing/src/main/java/com/hank/rabbitmq/routing/ReceiveLogsDirect.java
+++ routing/src/main/java/com/hank/rabbitmq/routing/ReceiveLogsDirect.java
@@ -1,8 +1,6 @@
 package com.hank.rabbitmq.routing;
 
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.*;
 
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
@@ -20,7 +18,32 @@ public class ReceiveLogsDirect {
         Connection connection = factory.newConnection();
         Channel channel = connection.createChannel();
 
+        channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.DIRECT);
+        String queueName = channel.queueDeclare().getQueue();
 
+        if (args.length < 1) {
+            System.err.println("Usage: ReceiveLogsDirect [info] [warning] [error]");
+            System.exit(1);
+        }
+        /**
+         * bind queue and exchange with routing keys
+         */
+        for (String severity : args) {
+            channel.queueBind(queueName, EXCHANGE_NAME, severity);
+        }
+
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+        Consumer consumer = new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body)
+                    throws IOException {
+                super.handleDelivery(consumerTag, envelope, properties, body);
+                String message = new String(body, "UTF-8");
+                System.out.println(" [x] Received '" + envelope.getRoutingKey() + "':'" + message + "'");
+            }
+        };
+        channel.basicConsume(queueName, true, consumer);
     }
 
 }
